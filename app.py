cbe166fa8044d4934fe2668acf8368dc534628fe
Satya Uday <97295777+satyauday112@users.noreply.github.com>
2025-06-27 22:34:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import datetime
 from typing import List, Optional, Annotated
 import db as db
 
-SECRET_KEY = "83daa0256a2289b0fb23693bf1f6034d44396675749244721a2b20e896e11662"
+SECRET_KEY = "Your Secret"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 45
 
